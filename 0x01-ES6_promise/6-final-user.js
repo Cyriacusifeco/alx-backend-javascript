917d2a54b17a5caab5bfd0c6c99d26710f10b979
Cyriacusifeco <cyriacusifeco@gmail.com>
2023-06-27 19:12:39 +0000

    return array

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,7 +4,7 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((result) => (
-      res.map((item) => ({
+      result.map((item) => ({
         status: item.status,
         value: item.status === 'fulfilled' ? item.value : String(item.reason),
       }))
