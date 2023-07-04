dcb44a8f0ce0d1b57fbc3efce7d1af15ded13cc8
Cyriacusifeco <cyriacusifeco@gmail.com>
2023-07-04 03:46:02 +0000

    clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,10 @@
+function cleanSet(set, startString) {
+  if (!set || !startString || !(set instanceof Set) || (typeof startString !== 'string')) {
+    return '';
+  }
+  const filteredValues = [...set].filter(value => value.startsWith(startString));
+  const cleanedString = filteredValues.map(value => value.slice(startString.length)).join('-');
+  return cleanedString;
+}
+
+export default cleanSet;
