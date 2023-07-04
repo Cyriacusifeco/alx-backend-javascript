8f841a11188eca51d643a60eb43ef7d0121d512b
Cyriacusifeco <cyriacusifeco@gmail.com>
2023-07-04 03:01:03 +0000

    update grade

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,13 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  const updatedStudents = students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObject = newGrades.find((grade) => grade.studentId === student.id);
+      return {
+        ...student,
+        grade: gradeObject ? gradeObject.grade : 'N/A'
+      };
+    });
+
+  return updatedStudents;
+}
