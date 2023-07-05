7e30a1a4ce48e0a8518c497a3f1f40a202c12e3f
Cyriacusifeco <cyriacusifeco@gmail.com>
2023-07-05 17:21:08 +0000

    hot fix

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,8 +2,8 @@ function cleanSet(set, startString) {
   if (!set || !startString || !(set instanceof Set) || (typeof startString !== 'string')) {
     return '';
   }
-  const filteredValues = [...set].filter(value => value.startsWith(startString));
-  const cleanedString = filteredValues.map(value => value.slice(startString.length)).join('-');
+  const filteredValues = [...set].filter((value) => value.startsWith(startString));
+  const cleanedString = filteredValues.map((value) => value.slice(startString.length)).join('-');
   return cleanedString;
 }
 
