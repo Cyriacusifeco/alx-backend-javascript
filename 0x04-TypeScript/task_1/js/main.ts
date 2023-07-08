bac908934bb01afb7f793b13415282fd787143b7
Cyriacusifeco <cyriacusifeco@gmail.com>
2023-07-08 11:54:11 +0000

    bundle all

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -2,9 +2,45 @@ interface Teacher {
   firstName: string;
   lastName: string;
   readonly fullTimeEmployee: boolean;
-  yearsOfExperience?: number;
+  yearsOfExperience: number;
   location: string;
   [key: string]: any;
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+interface StudentConstructor {
+  firstName: string;
+  lastName: string;
+}
+
+interface Student {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements Student {
+  private firstName: string;
+  private lastName: string;
+
+  constructor({ firstName, lastName }: StudentConstructor) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
 }
 
 const teacher3: Teacher = {
@@ -16,3 +52,18 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  fullTimeEmployee: true,
+  location: 'London',
+  numberOfReports: 17,
+};
+
+console.log(director1);
+
+function printTeacher(firstName: string, lastName: string): string {
+  const firstInitial = firstName.charAt(0);
+  return `${firstInitial}. ${lastName}`;
+}
