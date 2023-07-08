f6a49accd9f22f1f9b6021e9932ae5715e1284e1
Cyriacusifeco <cyriacusifeco@gmail.com>
2023-07-08 10:30:44 +0000

    create interface student

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,29 @@
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+const studentsList: Student[] = [
+  { firstName: "John", lastName: "Doe", age: 20, location: "New York" },
+  { firstName: "Jane", lastName: "Smith", age: 22, location: "London" },
+];
+
+const table = document.createElement("table");
+
+studentsList.forEach((student) => {
+  const row = document.createElement("tr");
+
+  const firstNameCell = document.createElement("td");
+  firstNameCell.textContent = student.firstName;
+  row.appendChild(firstNameCell);
+
+  const locationCell = document.createElement("td");
+  locationCell.textContent = student.location;
+  row.appendChild(locationCell);
+
+  table.appendChild(row);
+});
+
+document.body.appendChild(table);
