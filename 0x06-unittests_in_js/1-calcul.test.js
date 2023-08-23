c3cee8468d425dc05d28f6628fd28166756ca3f1
Cyriacusifeco <cyriacusifeco@gmail.com>
2023-08-23 10:20:14 +0000

    Combine descriptions

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const calculateNumber = require('./1-calcul.js');
+
+describe('calculateNumber', function() {
+  it('should perform SUM operation', function() {
+    assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+  });
+
+  it('should perform SUBTRACT operation', function() {
+    assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+  });
+
+  it('should perform DIVIDE operation', function() {
+    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+  });
+
+  it('should return "Error" for DIVIDE operation when divisor is 0', function() {
+    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+  });
+});
