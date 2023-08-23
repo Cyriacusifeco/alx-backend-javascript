13cb663d4499c224a0fa30152b330f93464e985d
Cyriacusifeco <cyriacusifeco@gmail.com>
2023-08-23 10:35:18 +0000

    Create Spies

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,16 @@
+const sinon = require('sinon');
+const assert = require('assert');
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', function() {
+  it('should call Utils.calculateNumber with correct arguments', function() {
+    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+
+    sendPaymentRequestToApi(100, 20);
+
+    assert(calculateNumberSpy.calledOnceWith('SUM', 100, 20));
+
+    calculateNumberSpy.restore();
+  });
+});
